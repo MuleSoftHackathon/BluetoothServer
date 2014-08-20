378784bb8137a9004c688ff3f7cce7d1084fcc8b
Sung Chiu <sung.chiu@mulesoft.com>
2014-08-20 15:09:16 -0700

    sphero and app change

--- app/app.js
+++ app/app.js
@@ -4,11 +4,28 @@ var express = require('express'),
     http = require('http'),
     app = express();
 
-
 var server = app.listen(8352,  function() {
-    console.log('Listening on port %d', server.address().port);
+    console.log('-------------Local Server Start-------------');
 });
 
+var options = {
+    hostname: process.argv[2],
+    port: 8080,
+    path: '/remoteDevice',
+    headers: {'Content-Type': 'application/json'},
+    method: 'POST'
+};
+
+var req = http.request(options, function(res) {
+    console.log('Connect to central server');
+});
+
+req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+});
+
+// write data to request body
+req.end(JSON.stringify({device_id: 'Team1Key', device_type: 'bt', port: 8352}));
 
 app.get('/', router.rootHandler);
 app.get('/sphero/:id/:action', router.spheroHandler);
--- app/sphero/sphero.js
+++ app/sphero/sphero.js
@@ -1,110 +1,173 @@
 'use strict';
 
+
+/**
+ * import modules
+ * sphero should be a singleton instance per bluetooth server
+ */
 var Sphero = require('./spheroApi.js'),
     sphero = new Sphero('/dev/cu.Sphero-RBW-RN-SPP'),
-    exports = module.exports,
-    pingPolling = function(){
+    exports = module.exports;
+
+
+/**
+ * declare other variables
+ */
+var pingPolling = function(){
         sphero.ping(function(){
         });
         if( sphero.isConnected ) {
             setTimeout(pingPolling, 30000);
         }
-    };
+    }, // periodically ping the sphero to prevent it going to sleeping mode
+    preAction, // record the previous action (have to sethead before rolling)
+    notification = [], // notification message from sphero
+    message; // message from sphero (ex. angle)
 
+/**
+ * register listeners
+ */
 sphero.on('notification', function(msg) {
-    console.log('Notification: ' + JSON.stringify(msg));
+    notification = [];
+    for( var i = 0; i < msg.DATA.length; i++ ) {
+        notification.push(msg.DATA[i]);
+    }
 });
 
 sphero.on('message', function(msg) {
-    console.log('Message: ' + JSON.stringify(msg));
+    console.log('MSG: ' + JSON.stringify(msg));
+    message = msg;
 });
 
+sphero.on('close', function(msg) {
+    console.log('Sphero Close');
+    sphero.isConnected = false;
+});
+
+sphero.on('error', function(msg) {
+    console.log('Sphero Error');
+    sphero.isConnected = false;
+});
+
+
+/**
+ * @param request
+ * @param response
+ * perform the corresponding request
+ */
 exports.action = function(request, response) {
-    console.log('New Request');
+    console.log('Sphero request: ' + request.path);
+
     var id = request.params.id,
         action = request.params.action,
         query = request.query,
         angle,
         speed,
         color = {},
-        msgCallback = function(e){
+        onFinished = function(e){
             response.writeHead(200);
-            if( e ) {
-                response.end(JSON.stringify({data: 'Sphero fails to perform action'}));
+            if(e) {
+                console.log(request.query);
+                response.end(JSON.stringify({status:'error', message: 'Sphero fails to perform action'}));
                 return;
             }
             sphero.isConnected = true;
-            response.end(JSON.stringify({data: 'Sphero ' + action}));
+            response.end(JSON.stringify({status:'ok', message: 'Sphero ' + action}));
         };
 
+
+    /**
+     * connecting to sphero
+     */
     if( action === 'connect' ) {
         if( !sphero.isConnected ) {
             sphero.connect(function(e){
                 response.writeHead(200);
-                if( e ) {
-                    response.end(JSON.stringify({data: 'Sphero fails to connect :' + e}));
+                if(e) {
+                    response.end(JSON.stringify({status:'error', message: 'Sphero fails to connect :' + e}));
                     return;
                 }
                 sphero.isConnected = true;
-                response.end(JSON.stringify({data: 'Sphero connected'}));
+                response.end(JSON.stringify({status:'ok', message: 'Sphero connected'}));
                 sphero.setDataStreaming([sphero.sensors.gyro_z], 1, 1);
                 pingPolling();
             });
         } else {
-            response.status(200).send(JSON.stringify({data: 'Sphero connected'}));
+            response.status(200).send(JSON.stringify({status:'ok', message: 'Sphero connected'}));
         }
         return;
     }
 
+    /**
+     * closing sphero
+     */
     if( action === 'close' ) {
         if( sphero.isConnected ) {
             sphero.close(function(e){
                 response.writeHead(200);
                 if(e) {
-                    response.end(JSON.stringify({data: 'Sphero fails to close :' + e}));
+                    response.end(JSON.stringify({status:'error', message: 'Sphero fails to close :' + e}));
                     return;
                 }
                 sphero.isConnected = false;
-                response.end(JSON.stringify({data: 'Sphero closed'}));
+                response.end(JSON.stringify({status:'ok', message: 'Sphero closed'}));
             });
         } else {
-            response.status(200).send(JSON.stringify({data: 'Sphero closed'}));
+            response.status(200).send(JSON.stringify({status:'ok', message: 'Sphero closed'}));
         }
         return;
     }
 
+
+    /**
+     * actions is valid only if the connection is on
+     */
     if( action && sphero.isConnected ) {
         switch (action) {
             case 'rotate':
-                angle = parseInt(query.angle);
-                if( angle && 0 <= angle && angle <= 360) {
-                    sphero.setHeading(angle, function(){
-                        sphero.setHeading(1, msgCallback);
-                    });
+                angle = parseInt(query.angle) + 180;
+                if( angle !== undefined && 0 <= angle && angle < 360) {
+                    preAction = 'rotate';
+                    sphero.setHeading(angle, onFinished);
                     return;
                 }
                 break;
+
             case 'color':
                 color.r = parseInt(query.r);
                 color.g = parseInt(query.g);
                 color.b = parseInt(query.b);
-                if( color.r && color.g && color.b && 0 <= color.r && color.r <= 255
-                    && 0 <= color.g && color.g <= 255 && 0 <= color.b && color.b <= 255) {
-                    sphero.setRGBLED(color.r, color.g, color.b, false, msgCallback);
+                if( color.r !== undefined && color.g !== undefined && color.b !== undefined && 0 <= color.r && color.r < 256
+                    && 0 <= color.g && color.g < 256 && 0 <= color.b && color.b < 256) {
+                    sphero.setRGBLED(color.r, color.g, color.b, false, onFinished);
                     return;
                 }
                 break;
+
             case 'move':
                 angle = parseInt(query.angle);
                 speed = parseInt(query.speed);
-                console.log(angle);
-                if( angle && speed && 0 <= angle && angle <= 360 && 0 <= speed && speed <= 100 ) {
-                    sphero.roll(angle, speed/100, msgCallback);
+                if( angle !== undefined && speed !== undefined && 0 <= angle && angle < 360 && 0 <= speed && speed < 100 ) {
+                    if( preAction === 'rotate' ) {
+                        sphero.setHeading(0, function(){
+                            sphero.roll(angle, speed/100, onFinished);
+                        });
+                        preAction = '';
+                    }else {
+                        sphero.roll(angle, speed / 100, onFinished);
+                    }
                     return;
                 }
                 break;
+
+            case 'sensing':
+                response.status(200).send(JSON.stringify({status:'ok', message: notification}));
+                return;
         }
     }
 
-    response.status(200).end(JSON.stringify({data: 'Command Not Valid'}));
+    /**
+     * if the action is not valid
+     */
+    response.status(200).end(JSON.stringify({status:'error', message: 'Command Not Valid'}));
 };
